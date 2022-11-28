4830022df38f554a2ba6ce2719d3ac5c22902780
BjarkeKarlsen <37935745+BjarkeKarlsen@users.noreply.github.com>
2022-11-28 16:16:32 +0100

    Add dto to citizen

---
+++ Handin3/RFMS_v3/RFMS_v3_App/Models/Dto/CitizenNoIdDto.cs
@@ -0,0 +1,12 @@
+﻿namespace RFMS_v3_App.Models.Dto
+{
+    public class CitizenNoIdDto
+    {
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public string Email { get; set; } = "";
+        public string PhoneNumber { get; set; } = "";
+        public string Category { get; set; } = "";
+        public int? CVR { get; set; }
+    }
+}
