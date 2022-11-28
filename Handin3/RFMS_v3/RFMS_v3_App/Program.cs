f7630b706a6e14bcd844f705ee48bf9edd8f8730
BjarkeKarlsen <37935745+BjarkeKarlsen@users.noreply.github.com>
2022-11-28 16:02:21 +0100

    Fixed connection string to use the mongoDb

--- Handin3/RFMS_v3/RFMS_v3_App/Program.cs
+++ Handin3/RFMS_v3/RFMS_v3_App/Program.cs
@@ -1,13 +1,22 @@
+using RFMS_v3_App.Models;
+using RFMS_v3_App.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var connectionString = builder.Configuration["ConnectionString"];
+//var connectionString = builder.Configuration["ConnectionString"];
+builder.Services.Configure<MongoDbSettings>(
+    builder.Configuration.GetSection("MongoDB"));
 
+builder.Services.AddSingleton<CitizenDbService>();
+
+
+builder.Services.AddControllers();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
